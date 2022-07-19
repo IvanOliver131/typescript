bb0a90c94573913007ffce8fd12f01d998ab63a2
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-07-19 15:10:14 -0300

    devCommit

---
+++ src/CreateCourseService.ts
@@ -0,0 +1,11 @@
+/**
+ * name -string
+ * duration - number
+ * educator - string
+ */
+
+class CreateCourseService {
+  execute(name: string, duration: number, educator: string) {
+    console.log(name, duration, educator);
+  }
+}
---
+++ src/routes.ts
@@ -0,0 +1,3 @@
+import { Request, Response } from "express";
+
+export function createCurse(request: Request, response: Response) {}
